64b00ccdafa7aebc00719be83240bce28b4be145
FluFFka <iliasfedorenko@mail.ru>
2022-04-06 16:06:59 +0300

    Delete comments

--- objects/main/Entity.py
+++ objects/main/Entity.py
@@ -14,24 +14,8 @@ class Entity(pygame.sprite.Sprite):
             self.direction = self.direction.normalize()
 
         self.rect.x += self.direction.x * self.speed
-        # self.collision('horizontal')
         self.rect.y += self.direction.y * self.speed
-        # self.collision('vertical')
 
     def collision(self,direction):
-        # if direction == 'horizontal':
-        #     for sprite in self.obstacle_sprites:
-        #         if sprite.rect.colliderect(self.rect):
-        #             if self.direction.x > 0: # moving right
-        #                 self.rect.right = sprite.rect.left
-        #             if self.direction.x < 0: # moving left
-        #                 self.rect.left = sprite.rect.right
 
-        # if direction == 'vertical':
-        #     for sprite in self.obstacle_sprites:
-        #         if sprite.rect.colliderect(self.rect):
-        #             if self.direction.y > 0: # moving down
-        #                 self.rect.bottom = sprite.rect.top
-        #             if self.direction.y < 0: # moving up
-        #                 self.rect.top = sprite.rect.bottom
         pass
