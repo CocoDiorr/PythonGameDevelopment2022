bec618ebeb9e97b5b8695c98fb8eae305302ce0f
yura-leb <lebedinsliyyue@gmail.com>
2022-06-10 17:13:14 +0300

    Entity scaling

--- objects/enemy/Swordsman.py
+++ objects/enemy/Swordsman.py
@@ -6,6 +6,6 @@ from config.Config import *
 class Swordsman(Enemy):
     """ """
     def __init__(self, level, position):
-        super().__init__(level, (level.visible, level.entity), SWORDSMAN_PATH, position, SWORDSMAN_ABS_ACCEL, SWORDSMAN_MAX_SPEED, SWORDSMAN_HEALTH, SWORDSMAN_ATTACK_RADIUS, SWORDSMAN_NOTICE_RADIUS)
+        super().__init__(level, (level.visible, level.entity), SWORDSMAN_PATH, SWORDSMAN_SIZE, position, SWORDSMAN_ABS_ACCEL, SWORDSMAN_MAX_SPEED, SWORDSMAN_HEALTH, SWORDSMAN_ATTACK_RADIUS, SWORDSMAN_NOTICE_RADIUS)
         self.equip_weapon(ColdSteel(level, (level.visible, level.cold_steels), SWORD_SPRITE_PATH, SWORD_SIZE, self, SWORD_DISTANCE, SWORD_COOLDOWN, SWORD_DAMAGE))
 
