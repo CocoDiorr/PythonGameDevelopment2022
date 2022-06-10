6db67b6fed4b877f25d5fe33d13459205c75f429
CocoDiorr <tarapelikan@gmail.com>
2022-06-10 03:18:12 +0300

    Added alpha version of start menu (no settings yet)
    * Added more pictures for start menu
    * Added hovered buttons pics
    * Added to Game states for start menu and play state
    * Added more texts for companion

--- game/Game.py
+++ game/Game.py
@@ -2,7 +2,7 @@ import pygame
 import pygame.display
 from config.Config import *
 from level.Level import Level
-
+from menu.StartMenu import StartMenu
 
 class Game:
     def __init__(self):
@@ -11,6 +11,8 @@ class Game:
         self.running = True
         self.locale = 'ru'
         self.level = Level(self.locale)
+        self.start_menu = StartMenu(self)
+        self.game_state = "start" # "play"
 
     def run(self):
         while self.running:
@@ -19,16 +21,26 @@ class Game:
             for event in events:
                 if event.type == pygame.QUIT:
                     self.running = False
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_h:
-                        self.level.companion_call()
+                        if self.game_state == "play":
+                            self.level.companion_call()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        self.level.buttons_event = event
+                        if self.game_state == "start":
+                            self.start_menu.buttons_event = event
+                        if self.game_state == "play":
+                            self.level.buttons_event = event
+
+                #elif self.game_state == "start":
 
 
 
             dt = self.clock.tick(FPS) / 1000
             self.screen.fill(BACKGROUND_COLOR)
-            self.level.run(dt)
+            if self.game_state == "start":
+                self.start_menu.display()
+            if self.game_state == "play":
+                self.level.run(dt)
             pygame.display.update()
