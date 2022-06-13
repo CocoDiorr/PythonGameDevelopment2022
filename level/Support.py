d7134a80f284b589ec8e156703310bc0de3a64f4
Purple_snake <rizvanzaidullin@gmail.com>
2022-06-13 19:19:05 +0300

    Docstrings added for Game and Level folders

--- level/Support.py
+++ level/Support.py
@@ -9,6 +9,7 @@ def import_csv_layout(path: str) -> list[list[str]]:
     Import terrain objects from the csv file.
 
     :param path: str: path to map's layer csv file
+    :return terrain_map: list of lists of data, which means if the tile on the layer of the map contains something
 
     """
     terrain_map = []
@@ -19,16 +20,17 @@ def import_csv_layout(path: str) -> list[list[str]]:
         return terrain_map
 
 
-def import_folder(path):
+def import_folder(path: str) -> dict[str, pygame.image]:
     """
     Import images from the folder.
 
     :param path: str: path to the folder with image.
+    :return surf_dict: dictionary of pygame.image with the key of filename converted to string
 
     """
-    surf_list = {}
+    surf_dict = {}
     for _, __, img_files in walk(path):
         for image in img_files:
             full_path = os.path.join(path, image)
-            surf_list[image] = pygame.image.load(full_path).convert_alpha()
-    return surf_list
+            surf_dict[image] = pygame.image.load(full_path).convert_alpha()
+    return surf_dict
